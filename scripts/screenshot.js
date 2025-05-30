7f109f3318b3879bdb152182bdf19c166b79ee62
Joaquimtxt <158314249+Joaquimtxt@users.noreply.github.com>
2025-05-30 17:34:24 -0300

    Update screenshot.js

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const path = require('path');
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] }); // <-- importante!
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], 
+  });
   const page = await browser.newPage();
-  await page.goto(`file://${process.cwd()}/docs/languagetable.html`);
+  const filePath = `file://${path.resolve(__dirname, '../docs/languagetable.html')}`;
+  await page.goto(filePath, { waitUntil: 'networkidle0' });
   await page.screenshot({ path: 'assets/languagetable.png', fullPage: true });
   await browser.close();
 })();
